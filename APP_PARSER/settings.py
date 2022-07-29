4daefbe103431d59872625f9fa24e34d0daef561
doctorart666 <55849321+doctorart666@users.noreply.github.com>
2022-07-29 13:07:43 +0300

    eleven commit

--- APP_PARSER/settings.py
+++ APP_PARSER/settings.py
@@ -9,8 +9,8 @@ import django_on_heroku
 #django_on_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-#BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'APP_PARSER.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': f'{BASE_DIR} / db.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
