4b78da212caa7e1082ba6b809ae8b9b9d61cf05b
doctorart666 <55849321+doctorart666@users.noreply.github.com>
2022-07-29 11:41:20 +0300

    six commit

--- APP_PARSER/settings.py
+++ APP_PARSER/settings.py
@@ -3,8 +3,10 @@ import os
 from pathlib import Path
 
 
-import django_heroku
-import dj_database_url
+#import django_heroku
+#import dj_database_url
+import django_on_heroku
+django_on_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
